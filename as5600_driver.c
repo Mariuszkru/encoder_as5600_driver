638d0be96d0b57e056d7add7eb1ae03c3bc44e0f
Mariusz Krukowski <mariusz.krukowski@aol.com>
2025-04-12 17:25:48 +0200

    read angle

--- as5600_driver.c
+++ as5600_driver.c
@@ -23,7 +23,19 @@
 #include "..\..\build\include\sdkconfig.h"
 
 
-
+#include "mk_i2c.h"
 #include "as5600_driver.h"
 
+
+void as5600_read_angle(uint16_t* angle) {
+    uint8_t data[2] = { 0 };
+    esp_err_t ret = i2c_dev_read(0, AS5600_ADDRESS, AS5600_REG_RAW_ANGLE, 2, data, 2);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to read angle: %s", esp_err_to_name(ret));
+        return -1;
+        }
+
+    *angle = (data[0] << 8) | data[1];
+    return 0;
+    }
 
